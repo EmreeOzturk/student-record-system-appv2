6286bb82510067a1c2d1f36748298f5aa9fdd27d
Emre <emreeozturk42@gmail.com>
2023-06-07 05:09:33 +0300

    admin panel's icons added

--- pages/admin/panel.tsx
+++ pages/admin/panel.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button, Grid, Text } from '@geist-ui/core';
 import Link from 'next/link';
+import { HiUserAdd } from 'react-icons/hi';
+import { BsFillPeopleFill } from 'react-icons/bs';
 const AdminPanel = () => {
     return (
         <Grid
@@ -14,14 +16,36 @@ const AdminPanel = () => {
         >
             <Button h={4} type="success">
                 <Link href="/admin/add-teacher">
-                    <Text color="white" h4>
+                    <Text
+                        color="white"
+                        h4
+                        style={{
+                            display: 'flex',
+                            gap: '.3rem',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            fontSize: '1.8rem',
+                        }}
+                    >
+                        <HiUserAdd />
                         Add Teacher
                     </Text>
                 </Link>
             </Button>
             <Button h={4} type="warning">
                 <Link href="/admin/view-students">
-                    <Text color="white" h4>
+                    <Text
+                        color="white"
+                        h4
+                        style={{
+                            display: 'flex',
+                            gap: '.3rem',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            fontSize: '1.8rem',
+                        }}
+                    >
+                        <BsFillPeopleFill />
                         View Students
                     </Text>
                 </Link>
