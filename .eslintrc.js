4d9e96dab207d3ebd8cc47c14f1c16649fc1bd7e
Emre <emreeozturk42@gmail.com>
2023-06-07 05:30:24 +0300

    eslint updated 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,14 +28,11 @@ module.exports = {
     plugins: ['@typescript-eslint'],
     extends: [
         'next/core-web-vitals',
-        'plugin:@typescript-eslint/recommended',
         'prettier',
-        'plugin:jsx-a11y/recommended',
         'plugin:security/recommended',
     ],
 
     rules: {
-        'react/react-in-jsx-scope': 'off',
         'react/jsx-filename-extension': [
             1,
             {
@@ -44,7 +41,6 @@ module.exports = {
         ],
         'react/no-unescaped-entities': 'off',
         '@next/next/no-page-custom-font': 'off',
-        'react/jsx-props-no-spreading': 'off',
         'import/extensions': [
             'error',
             'ignorePackages',
@@ -53,14 +49,6 @@ module.exports = {
                 jsx: 'never',
                 ts: 'never',
                 tsx: 'never',
-            },
-        ],
-        'jsx-a11y/anchor-is-valid': [
-            'error',
-            {
-                components: ['Link'],
-                specialLink: ['hrefLeft', 'hrefRight'],
-                aspects: ['invalidHref', 'preferButton'],
             },
         ],
         'no-nested-ternary': 'off',
